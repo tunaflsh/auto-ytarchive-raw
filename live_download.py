384bae0cabf230f37b0b59be50051f6cb28212d5
Sam Phung <sphung@uncc.edu>
2023-05-06 12:54:09 -0400

    Allow a means to be able to use cookies incase the stream requires login

--- live_download.py
+++ live_download.py
@@ -2,19 +2,21 @@ import subprocess
 import const
 
 
-def download(video_id):
+def download(video_id, require_cookie):
     setDownloaded = False
     # Download if download process hasn't already been initiated
     # change back to /k
-    command_list = ['start', 'cmd', '/k']
-    command_list += ['ytarchive.exe', '-o',
-                     const.DOWNLOAD,
-                     '--add-metadata', '-t', '--vp9', '--write-description', '--write-thumbnail', '--threads', '2',
+    command_list = ['start', f'ytarchive {video_id}', '/min', 'cmd', '/c']
+    command_list += ['ytarchive.exe', '-v']
+    if require_cookie and const.COOKIE:
+        command_list += ['-c', const.COOKIE]
+    command_list += ['-o', const.DOWNLOAD,
+                     '--add-metadata', '-t', '--vp9', '--mkv', '--write-description', '--write-thumbnail', '--threads', '2',
                      '-w']
     command_list += [f'https://www.youtube.com/watch?v={video_id}', 'best']
 
     try:
-        print("[INFO] Downloading Live Stream")
+        print(f"[INFO] Downloading Live Stream {video_id}")
         output = subprocess.run(command_list, check=True, shell=True)
         # If theres an error then this ensures a redownload, but only works if the program crashes by itself immediately
         # print("[Debug]Output: ", output)
--- private_download.py
+++ private_download.py
@@ -6,14 +6,19 @@ import const
 def download(files):
     for file in reversed(files):
         if '.json' in file:
-            file = os.getcwd() + '\\' + file
-            command_list = f'start cmd /k ytarchive-raw-go -t {const.PDOWNLOAD_THREADS} -i "{file} -o {const.PDOWNLOAD}"'
+            file_path = os.getcwd() + '\\' + file
+            command_list = ['start', f'ytarchive-raw-go {file[6:-5]}', '/min', 'cmd', '/k', 'ytarchive-raw-go.exe', '-t', str(const.PDOWNLOAD_THREADS)]
+            command_list += ['-i', file_path, '-o', const.PDOWNLOAD]
+            # command_list = f'start cmd /k ytarchive-raw-go -t {const.PDOWNLOAD_THREADS} -i "{file_path} -o {const.PDOWNLOAD}"'
             try:
-                print("[INFO] Downloading Video")
-                output = subprocess.run(command_list, shell=True)
+                print(f"[INFO] Downloading Privated Video {file}")
+                output = subprocess.run(command_list, shell=True, start_new_session=True)
                 # If theres an error then this ensures a redownload, but only works if the program crashes by itself immediately
                 # print("[Debug] Immediate Return Code:", output.returncode)
             except Exception as e:
+                print('[ERROR]', e)
                 print("[INFO] Retry Download")
                 output = subprocess.run(command_list)
+            finally:
+                continue
 
